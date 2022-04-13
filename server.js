ab0d3c373f37e6dc76fb037ec9314472553d08db
clarkmoniz <clarkamoniz@gmail.com>
2022-04-13 15:30:35 -0700

    Add static data.json file, setup express and bodyparser, server.js file

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+
+
+
+
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
